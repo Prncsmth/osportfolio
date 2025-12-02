6c2f8a26058e7ab081036d7645d8b667cf576486
Smith Degamo <smithdegamox2@gmail.com>
2025-12-03 03:13:58 +0800

    sadas

--- my-react-app/src/pages/home.jsx
+++ my-react-app/src/pages/home.jsx
@@ -44,28 +44,7 @@ export default function Home() {
           Here are a few favorites.
         </p>
 
-        <div className="project-grid">
-          <div className="project-card">
-            <div className="project-img"></div>
-            <h3>Building Reusable React Components Using Props and List Rendering</h3>
-            <p>This project teaches how to create reusable React components that get their data from props. You’ll store multiple items in an array and use .map() to display them. It’s a practical way to learn how real websites show lists of products, posts, or users.</p>
-            <div className="tags"><span>React</span><span>JS</span></div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-img"></div>
-            <h3>React E-Commerce Web Application Using DummyJSON API</h3>
-            <p>This project requires building a React-based online shop that gets product data from the DummyJSON API. Users should be able to view all products, open a product’s details, use search and filters, and manage a cart up to checkout.</p>
-            <div className="tags"><span>React</span><span>JS</span><span>CSS</span></div>
-          </div>
-
-          <div className="project-card">
-            <div className="project-img"></div>
-            <h3>Profile Page</h3>
-            <p>A user-centered profile page that highlights essential personal information while maintaining a balanced visual hierarchy. The design uses consistent spacing, typography, and color to deliver a clear, intuitive, and aesthetically pleasing user experience.</p>
-            <div className="tags"><span>Figma</span><span>HTML</span><span>CSS</span><span>React</span></div>
-          </div>
-        </div>
+        
 
         <Link to="/projects" className="btn secondary view-all">View All Projects</Link>
       </section>
